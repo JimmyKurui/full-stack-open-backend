0caa51e7fef265156c6b1629cac2ea14fd901758
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2024-02-05 19:11:21 +0300

    Part3 3.7 - 3.8 complete

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
-app.use(express.json())
+app.use(express.json());
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'));
+
+morgan.token('post-body', function (req, res) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+  return '';
+});
 
 let persons = [
     { 
